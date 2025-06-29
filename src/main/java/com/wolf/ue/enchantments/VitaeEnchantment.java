3ed10a5840ea603d708beab38625d17c8f9403e1
Error1015 <156535541+Error1015@users.noreply.github.com>
2025-06-29 15:02:20 +0800

    添加 ParchmentMC 映射

--- src/main/java/com/wolf/ue/enchantments/VitaeEnchantment.java
+++ src/main/java/com/wolf/ue/enchantments/VitaeEnchantment.java
@@ -4,6 +4,7 @@ import com.mcmostwolf.enchantmentlib.api.BasicEnchantment;
 import com.wolf.ue.enchantments.init.EquipmentSlotsManager;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
+// 鲜活
 public class VitaeEnchantment extends BasicEnchantment {
     public VitaeEnchantment() {
         super(EnchantmentCategory.ARMOR, EquipmentSlotsManager.ARMORS, Rarity.VERY_RARE, true, false, false, false, 5);
